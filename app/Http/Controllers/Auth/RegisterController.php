35ceb51fb7ffdb29520f27ccc59aea8b53a63247
derkiab <110829988+derkiab@users.noreply.github.com>
2023-05-11 20:52:48 -0400

    Reparado Register y implementacion de seleccion de Pais, region y ciudad de forma dinamica

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,11 +54,10 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'run' => ['required', 'unique:users','regex:/^\d{7,8}-[0-9K]$/'],
-            'nombreCalle' => ['required', 'string'],
-            'numeroCalle' => ['required', 'string'],
-            'city' => ['required', 'string'],
-            'region' => ['required', 'string'],
-            'country' => ['required', 'string'],
+            'address' => ['required', 'string'],
+            'city_fk' => ['required', 'string'],
+            'region_fk' => ['required', 'string'],
+            'country_fk' => ['required', 'string'],
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
@@ -73,19 +72,42 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
 {
-    $city = City::findOrFail($data['city']);
-    $region = $city->region;
-    $country = $region->country;
+
 
     return User::create([
         'run' => $data['run'],
         'name' => $data['name'],
         'email' => $data['email'],
         'password' => Hash::make($data['password']),
-        'city_fk' => $city->id,
-        'region_fk' => $region->id,
-        'country_fk' => $country->id,
+        'address' => $data['address'],
+        'city_fk' => $data['city_fk'],
+        'region_fk' => $data['region_fk'],
+        'country_fk' => $data['country_fk'],
+
+
     ]);
 
+}
+
+public function showRegistrationForm()
+{
+    $countries = Country::all();
+    $regions= Region::all();
+    $cities = City::all();
+    return view('auth.register', compact('cities','regions','countries'));
+}
+
+public function getRegions($countryId)
+{
+    $regions = Region::where('country_fk', $countryId)->get();
+
+    return response()->json(['regions' => $regions]);
+}
+
+public function getCities($regionId)
+{
+    $cities = City::where('region_fk', $regionId)->get();
+
+    return response()->json(['cities' => $cities]);
 }
 }
