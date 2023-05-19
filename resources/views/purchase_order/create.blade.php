89e75d6e4a763d3961a44fd5dc24c676b9802b4a
rainefpp <70712013+rainefpp@users.noreply.github.com>
2023-05-19 00:26:01 -0400

    arreglo de datatable

--- resources/views/purchase_order/create.blade.php
+++ resources/views/purchase_order/create.blade.php
@@ -13,6 +13,7 @@
 @endsection
 
 @section('css')
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
 @endsection
 
 @section('content')
@@ -206,7 +207,7 @@
 
 @section('js')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.js"></script>
-    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
