0b45108a0b49ed43299956e1af1bc333d1872605
derkiab <110829988+derkiab@users.noreply.github.com>
2023-05-23 23:58:16 -0400

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,7 +24,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('imagen');
+            $table->string('imagen')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
