cfaa597d810e5e5cfda4c4d65929286e891467d6
derkiab <110829988+derkiab@users.noreply.github.com>
2023-05-25 22:25:12 -0400

    City mantenedor (incompleto)

---
+++ resources/views/cities/index.blade.php
@@ -0,0 +1,281 @@
+@extends('layouts.argon.app')
+
+@section('title')
+    {{ 'Ciudades' }}
+@endsection
+
+@section('breadcrumb')
+    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Paginas</a></li>
+        <li class="breadcrumb-item text-sm text-white active" aria-current="page">Ciudades</li>
+    </ol>
+    <h6 class="font-weight-bolder text-white mb-0">Ciudades</h6>
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
+@endsection
+
+@section('content')
+    <div class="container-fluid py-4">
+        <div class="row">
+            <div class="col-12">
+                <div class="card mb-4 ps-3 pe-3 pt-2">
+                    <div class="card-header pb-0">
+                        <h6>Tabla de Ciudades</h6>
+                    </div>
+                    <div class="card-body px-0 pt-0 pb-2">
+                        <button class="btn btn-sm btn-outline-success ms-4" data-bs-toggle="modal"
+                            data-bs-target="#addCityModal">
+                            Añadir Ciudad
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="addCityModal" tabindex="-1" aria-labelledby="addModalLabel"
+                            aria-hidden="true" data-bs-backdrop="static">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="addModalLabel">Añadir Ciudad</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <form method="POST" id="addCityForm" action="{{ route('cities.store') }}">
+                                        @csrf
+
+                                        <div class="modal-body">
+                                            <div class="form-group">
+                                                <label for="name">Nombre Ciudad:</label>
+                                                <input type="text"
+                                                    class="form-control @error('name') is-invalid @enderror" id="name"
+                                                    name="name" required>
+                                                @error('name')
+                                                    <span class="invalid-feedback d-block" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+
+                                                <label for="region_fk">Región a que pertenece:</label>
+                                                    <select class="form-control form-select" id="region_fk"
+                                                        name="region_fk">
+                                                        @foreach ($regions as $region)
+                                                            <option value="{{ $region->id }}">
+                                                                {{ $region->name }}
+                                                            </option>
+                                                        @endforeach
+                                                    </select>
+                                                <label for="country_fk">País a que pertenece:</label>
+                                                    <select class="form-control form-select" id="country_fk"
+                                                        name="country_fk">
+                                                        @foreach ($countries as $country)
+                                                            <option value="{{ $country->id }}">
+                                                                {{ $country->name }}
+                                                            </option>
+                                                        @endforeach
+                                                    </select>
+                                            </div>
+
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-sm btn-outline-danger"
+                                                data-bs-dismiss="modal">Cerrar</button>
+                                            <button type="submit" class="btn btn-sm btn-outline-success">Añadir</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="table-responsive p-0">
+                            <table id="cities-table" class="table display table-stripped align-items-center">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">Nombre Ciudad</th>
+                                        <th class="text-center">País</th>
+                                        <th class="text-center">Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($cities as $region)
+                                        <tr>
+                                            <td class="text-center">{{ $region->name }}</td>
+                                            <td class="text-center">{{ $region->country->name }}</td>
+
+                                            <td class="text-center pt-3">
+                                                <button id="editButton"
+                                                    class="btn btn-sm btn-outline-primary edit-modal-btn"
+                                                    data-bs-toggle="modal" data-bs-target="#editModal"
+                                                    data-region-id="{{ $region->id }}"
+                                                    data-region-name="{{ $region->name }}">
+                                                    <i class="fa fa-edit"></i>
+                                                </button>
+                                                <form action="{{ route('cities.destroy', $region->id) }}" method="POST"
+                                                    style="display: inline;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit"
+                                                        class="btn btn-sm btn-outline-danger delete-region"
+                                                        data-id="{{ $region->id }}"><i class="fa fa-trash"
+                                                            aria-hidden="true"> Delete</i></button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+
+                            </table>
+                            <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel"
+                                aria-hidden="true" data-bs-backdrop="static">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="editModalLabel">Editar nombre Ciudad</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <form method="POST" id="editForm"
+                                            action="{{ route('cities.update', ['id' => '__ID__']) }}">
+                                            @csrf
+                                            @method('PUT')
+
+                                            <div class="modal-body">
+                                                <div class="form-group">
+                                                    <label for="editName">Ciudad:</label>
+                                                    <input type="text"
+                                                        class="form-control @error('name') is-invalid @enderror"
+                                                        id="editName" name="name"required>
+                                                    @error('name')
+                                                        <span class="invalid-feedback d-block" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                    <label for="country_fk">País al que pertenece:</label>
+                                                    <select class="form-control form-select" id="country_fk"
+                                                    name="country_fk">
+                                                    @foreach ($countries as $country)
+                                                        <option value="{{ $country->id }}">
+                                                            {{ $country->name }}
+                                                        </option>
+                                                    @endforeach
+                                                </select>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-sm btn-outline-danger"
+                                                    data-bs-dismiss="modal">Cerrar</button>
+                                                <button type="submit"
+                                                    class="btn btn-sm btn-outline-success">Editar</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('js')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
+    <script>
+        // Usar Ajax para manejar el envio del formulario del modal para añadir usuarios
+        var addCityForm = document.getElementById('addCityForm');
+        addCityForm.addEventListener('submit', function(event) {
+            event.preventDefault(); // Previene enviar de inmediato el form
+
+            var formData = new FormData(addCityForm);
+            var xhr = new XMLHttpRequest();
+            //usar xhr para manejar la respuesta del controlador
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    if (xhr.status === 200) {
+
+                        // se parsea a json debido a que el controlador entrega un json
+                        var response = JSON.parse(xhr.responseText);
+                        if (response.success) {
+                            // SE crea el usuario
+                            $('#addCityModal').modal('hide'); // se esconde el modal
+                            location.reload(); // se recarga al mismo tiempo que se esconde el modal
+                        } else {
+                            // muestra los errores
+                            displayErrors(response.errors);
+                        }
+                    } else {
+                        // Handle AJAX request error
+                        console.error('AJAX request error');
+                    }
+                }
+            };
+
+            xhr.open('POST', addCityForm.getAttribute('action'));
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            xhr.setRequestHeader('Accept', 'application/json');
+            xhr.setRequestHeader('X-CSRF-TOKEN', document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
+            xhr.send(formData);
+        });
+
+        // Funcion que muestra errores de validacion
+        function displayErrors(errors) {
+            // Limpia errores anteriores
+            $('.invalid-feedback').html('');
+
+            // Muestra los errores nuevos
+            for (var field in errors) {
+                var errorMessages = errors[field];
+                var errorField = $('#' + field);
+                errorField.addClass('is-invalid');
+                errorField.siblings('.invalid-feedback').html(errorMessages.join('<br>'));
+
+                var errorLabel = $('<span>').addClass('error-message text-danger').text(errorMessages.join(', '));
+                errorField.after(errorLabel);
+            }
+        }
+    </script>
+    <script>
+        $(document).ready(function() {
+            $('#cities-table').DataTable({
+                dom: 'lfrtip',
+
+                language: {
+                    url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
+                },
+
+
+            });
+        });
+        $('#addCityModal').on('hide.bs.modal', function() {
+
+            $('.error-message').remove();
+        });
+
+        // Clear error messages when the form is submitted successfully
+        addCityForm.addEventListener('submit', function() {
+            $('.invalid-feedback').html('');
+            $('.error-message').remove();
+        });
+
+        $('#editModal').on('show.bs.modal', function(event) {
+            const button = $(event.relatedTarget); // Button que triggerea el modal
+            const regionId = button.data('region-id');
+            const regionName = button.data('region-name');
+
+            const editForm = $('#editForm');
+
+
+            // Actualizar ID de la ruta
+            const actionUrl = editForm.attr('action').replace('__ID__', regionId);
+
+            editForm.attr('action', actionUrl);
+            const nameInput = $('#editName');
+            nameInput.val(regionName);
+            // Reemplazar el valor del nombre en el input el modal
+
+        });
+
+
+    </script>
+@endsection
