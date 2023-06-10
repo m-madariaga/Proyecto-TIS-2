8586db2fe389dd3f397d63332be06e034625f8cb
Nicoperez19 <nperez@ing.ucsc.cl>
2023-06-10 02:54:32 -0400

    arreglando detalles y modal perfil cliente

--- resources/views/profile_landing.blade.php
+++ resources/views/profile_landing.blade.php
@@ -1,18 +1,9 @@
 @extends('layouts-landing.welcome')
 
 @section('css')
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
 @endsection
 
 @section('js')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
-
     <script>
         $(document).ready(function() {
 
@@ -67,12 +58,6 @@
             $('#editProfileLandingModal').on('show.bs.modal', function() {
                 // Do something when the modal is shown
             });
-
-        });
-    </script>
-
-    <script>
-        $(document).ready(function() {
 
             $('#openModalButton').click(function() {
                 $('#editPasswordLandingModal').modal('show');
@@ -228,18 +213,16 @@
                                                 <td class="text-center">${{ $order->total }}</td>
                                                 <td class="text-center">
                                                     <button type="button"
-                                                        class="button_edit_profile btn btn-sm btn-rounded ms-2 mx-2 me-auto"
+                                                        class="view-order button_edit_profile btn btn-sm btn-rounded ms-2 mx-2 me-auto"
                                                         data-order-id="{{ $order->id }}">Ver Pedido</button>
 
                                                     <div class="d-none" id="order-details-{{ $order->id }}">
                                                         <table class="table">
-
                                                             <tbody>
                                                                 @foreach ($order->details as $detail)
                                                                     <tr>
                                                                         <td class="text-center">
                                                                             {{ $detail->product->nombre }}</td>
-
                                                                         <td class="text-center">{{ $detail->cantidad }}
                                                                         </td>
                                                                         <td class="text-center">${{ $detail->precio }}
