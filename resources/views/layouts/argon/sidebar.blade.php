86e552c03e6e9a2c9692a6ff3597cd3bfa23989d
rainefpp <70712013+rainefpp@users.noreply.github.com>
2023-06-21 23:34:42 -0400

    boton en sidebar
    se borro y lo agrege

--- resources/views/layouts/argon/sidebar.blade.php
+++ resources/views/layouts/argon/sidebar.blade.php
@@ -247,6 +247,17 @@
                     </a>
                 </li>
             @endcan
+            @can('mantenedor punto de venta')
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->is('page') ? 'active' : '' }}" href="{{ route('point_of_sale') }}">
+                        <div
+                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="ni ni-bullet-list-67 text-warning text-sm opacity-10"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Punto de venta</span>
+                    </a>
+                </li>
+            @endcan
         </ul>
     </div>
 </aside>
