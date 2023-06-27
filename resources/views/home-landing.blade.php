5ad23ae1ea3a07f7634d0d29fdc9c26d69a8c681
JaviMatus77 <jmatusm@ing.ucsc.cl>
2023-06-27 19:49:17 -0400

    Update home-landing.blade.php

--- resources/views/home-landing.blade.php
+++ resources/views/home-landing.blade.php
@@ -111,115 +111,30 @@
                         @endforeach
                     </div>
                 </div>
-
-
+                @endforeach
             </div>
+
+            <a class="carousel-control-prev" href="#recommendedCarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true" style="background-color: black; filter: invert(1);"></span>
+                <span class="sr-only">Anterior</span>
+            </a>
+            <a class="carousel-control-next" href="#recommendedCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true" style="background-color: black; filter: invert(1);"></span>
+                <span class="sr-only">Siguiente</span>
+            </a>
         </div>
     </div>
-
-
-
-    <!-- Deal of the week -->
-
-    <div class="sale" style="">
-        <div class="container fill_height">
-            <div class="row align-items-center fill_height ">
-                <div class="col-12 d-flex flex-column justify-content-center align-items-center text-center">
-                    <div class="sale_content align-items-center flex">
-                        <h1>Sección de Promociones</h1>
-                        <div class="productos-oferta justify-content-center align-middle flex">
-                            @if ($promociones->isNotEmpty())
-                            @foreach ($promociones as $promocion)
-                            <div class="product-item men flex-inline">
-                                <div class="product product_filter ">
-                                    <div class="product_image">
-                                        <img src="/assets/images/images-products/{{ $promocion->product->imagen }}" alt="">
-                                    </div>
-                                    <div class="favorite"></div>
-                                    <div class="product_info">
-                                        <h6 class="product_name"><a>{{$promocion->product->nombre}}</a>
-                                        </h6>
-                                        <div class="product_price">{{$promocion->product->precio}}</div>
-                                    </div>
-                                </div>
-                                <div class="red_button add_to_cart_button"><a href="#">Añadir al carrito</a>
-                                </div>
-                            </div>
-                            @endforeach
-                            <div class="red_button sale_button"><a href="#">Sale</a></div>
-                            @else
-                            <h3>No hay productos en promoción</h3>
-                            @endif
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <!-- Benefit -->
-
-    <div class="benefit mb-4">
-        <div class="container">
-            <div class="row benefit_row">
-                <div class="col-lg-4 benefit_col">
-                    <div class="benefit_item d-flex flex-row align-items-center">
-                        <div class="benefit_icon"><i class="fa fa-truck" aria-hidden="true"></i></div>
-                        <div class="benefit_content">
-                            <h6>ZONA DESPACHO</h6>
-                            <p>Para todas las regiones del país</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 benefit_col">
-                    <div class="benefit_item d-flex flex-row align-items-center">
-                        <div class="benefit_icon"><i class="fa fa-money" aria-hidden="true"></i></div>
-                        <div class="benefit_content">
-                            <h6>PAGO EFECTIVO</h6>
-                            <p>Excluivamente en Chillán y San Fernando</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 benefit_col">
-                    <div class="benefit_item d-flex flex-row align-items-center">
-                        <div class="benefit_icon"><i class="fa fa-clock-o" aria-hidden="true"></i></div>
-                        <div class="benefit_content">
-                            <h6>ENTREGA PRESENCIAL</h6>
-                            <p>Solo ciudad de Chillán y San fernando</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    @if (isset($results) && count($results) > 0)
-    <div class="container">
-        <h2>Search Results</h2>
-        <ul>
-            @foreach ($results as $result)
-            <li>{{ $result }}</li>
-
-            @endforeach
-    </div>
-
-    <a class="carousel-control-prev" href="#recommendedCarousel" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true" style="background-color: black; filter: invert(1);"></span>
-        <span class="sr-only">Anterior</span>
-    </a>
-    <a class="carousel-control-next" href="#recommendedCarousel" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true" style="background-color: black; filter: invert(1);"></span>
-        <span class="sr-only">Siguiente</span>
-    </a>
 </div>
 
 
-
-
-
+<a class="carousel-control-prev" href="#recommendedCarousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true" style="background-color: black; filter: invert(1);"></span>
+    <span class="sr-only">Anterior</span>
+</a>
+<a class="carousel-control-next" href="#recommendedCarousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true" style="background-color: black; filter: invert(1);"></span>
+    <span class="sr-only">Siguiente</span>
+</a>
 
 <!-- Deal of the week -->
 
@@ -241,7 +156,7 @@
                                 <div class="product_info">
                                     <h6 class="product_name"><a>{{$promocion->product->nombre}}</a>
                                     </h6>
-                                    <div class="product_price">{{$promocion->product->precio - $promocion->descuento}}</div>
+                                    <div class="product_price">{{$promocion->product->precio}}</div>
                                 </div>
                             </div>
                             <div class="red_button add_to_cart_button"><a href="#">Añadir al carrito</a>
@@ -259,6 +174,9 @@
         </div>
     </div>
 </div>
+
+
+
 
 
 <!-- Benefit -->
@@ -298,6 +216,7 @@
     </div>
 </div>
 
+
 @if (isset($results) && count($results) > 0)
 <div class="container">
     <h2>Search Results</h2>
@@ -305,7 +224,6 @@
         @foreach ($results as $result)
         <li>{{ $result }}</li>
         @endforeach
-    </ul>
 </div>
 @endif
 @endsection
