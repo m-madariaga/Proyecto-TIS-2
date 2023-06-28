b2c1346fd87a71dae04842eb06b0a887002d80e7
Nicoperez19 <nperez@ing.ucsc.cl>
2023-06-27 20:07:50 -0400

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -359,8 +359,7 @@
         @if(session('success'))
         Swal.fire({
             title: '¡Éxito!',
-            text: '{{ session('
-            success ') }}',
+            text: '{{ session('success') }}',
             icon: 'success',
             timer: 3000, // Tiempo en milisegundos (3 segundos)
             showConfirmButton: false
