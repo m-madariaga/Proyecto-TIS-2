41f63c43c188f9996b863430ab29c0c854665c6d
derkiab <110829988+derkiab@users.noreply.github.com>
2023-07-04 15:44:13 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -96,7 +96,7 @@
             </div>
         </div>
         <div class="row mt-4">
-            <div class="col-lg-6 mb-lg-0 mb-4">
+            <div class="col-lg-12 mb-lg-0 mb-4">
                 <div class="card z-index-2 h-100">
                     <div class="card-header pb-0 pt-3 bg-transparent">
                         <h6 class="text-capitalize">Resumen Ventas $</h6>
